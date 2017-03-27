5443cb72ded86ca0013fe854f1ef8829f30fc6f5
Do Huynh <admin@cartonplein.org>
2017-03-27 11:06:35 +0000

    remove hidden files

--- run.sh
+++ run.sh
@@ -1,1 +1,20 @@
-MONGO_URL=mongodb://cartonplein:papiervide@ds135830.mlab.com:35830/cartonplein_dev meteor run -p 8080
+env=$1
+
+Run () {
+   echo "Running app on: $2" 
+   MONGO_URL=mongodb://cartonplein:papiervide@$1/$2 meteor run -p 8080
+}
+
+case $env in
+    "DEV")
+    Run "ds135830.mlab.com:35830" "cartonplein_dev"
+    ;;
+    "STAG") 
+    echo "STAG"
+    Run "ds137530.mlab.com:37530" "cartonplein_stag"
+    ;;
+    "PROD") 
+    Run "ds137540.mlab.com:37540" "cartonplein_prod"
+    ;;
+esac
+
