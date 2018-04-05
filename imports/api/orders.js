3a850f3084bfbd3c92961d4fe9382f81625a34d8
Do Huynh <admin@cartonplein.org>
2018-04-05 19:59:16 +0000

    Add date filter > 2018

--- imports/api/orders.js
+++ imports/api/orders.js
@@ -2,13 +2,28 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
 import { SimpleRest } from 'meteor/simple:rest';
+import { moment } from 'meteor/momentjs:moment';
  
 export const Orders = new Mongo.Collection('orders');
 
 if (Meteor.isServer) {
   // This code only runs on the server
   Meteor.publish('orders', function() {
-    return Orders.find({});
+    const filterDay = moment("20180101", "YYYYMMDD");
+    
+    // Fetch unique dates
+    var dates = _.uniq(Orders.find({}, {
+        sort: {'shipping.date':1}, fields: {'shipping.date': true}
+      }).fetch().map(function(x) {
+        if (moment(x.shipping.date, "DD/MM/YYYY") >= moment("01/01/2018", "DD/MM/YYYY"))
+          {return x.shipping.date};
+        }), true);
+    // Sort dates
+    //console.log('dates:', dates);
+    
+    const query = {'shipping.date': { $in : dates } };
+    //console.log (query);
+    return Orders.find(query);
   });
 }
  
